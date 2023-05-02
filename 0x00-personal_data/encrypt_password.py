f658c1d4bd8079cec4ac3c5e0efbd4d05e470d42
gims-inc <gabrielmusanga@gmail.com>
2023-05-02 12:13:13 +0300

    encrypt pwd

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Handling of personal data  --encryption"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """function that expects one string argument name password and returns
+    a salted,hashed password, which is a byte string.
+    args:
+        pasword: (str)
+        returns: hashed password
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password.
+    args:
+        hashed_password: (bytes) hashed password
+        password: (str) provided password
+        returns: bool
+    """
+    return hash_password(password) == hashed_password
