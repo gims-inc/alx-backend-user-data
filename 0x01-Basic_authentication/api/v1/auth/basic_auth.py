7353087480c3306582ab6042b207aea1d2ee1e35
gims-inc <gabrielmusanga@gmail.com>
2023-05-04 12:15:57 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -18,7 +20,7 @@ class BasicAuth(Auth):
         Returns:
             None if authorization_header is None
             None if authorization_header is not a string
-            None if authorization_header doesn’t start by Basic
+            None if authorization_header doesn't start by Basic
             (with a space at the end)
             Otherwise, return the value after Basic (after the space)
         """
@@ -44,3 +46,50 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """Returns the user and password from the decoded Base64 string
+        Args:
+            decoded_base64_authorization_header (str)
+        Returns:
+            None, None if decoded_base64_authorization_header is None
+            None if decoded_base64_authorization_header is not a string
+            None if decoded_base64_authorization_header doesn’t contain :
+            user email and the user password separated by a :
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return decoded_base64_authorization_header.split(':')
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """Returns a User object from the user_email and user_pwd
+        Args:
+            user_email (str)
+            user_pwd (str)
+        Returns:
+            None if user_email is None or not a string.
+            None if user_pwd is None or not a string.
+            None if your database (file) doesn’t contain any.
+                User instance with email equal to user_email  all cases.
+            None if user_pwd is not the password of the User instance found.
+            Otherwise, return the User instance
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        user = User.search({'email': user_email})
+        if not user or len(user) == 0:
+            return None
+        user = user[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
