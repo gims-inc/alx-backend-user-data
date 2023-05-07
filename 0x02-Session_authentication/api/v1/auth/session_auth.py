c750b1649fca9a0e7c5a94ec39c4e41b994a7e62
gims-inc <gabrielmusanga@gmail.com>
2023-05-07 03:47:33 +0300

    session auth setup

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Session Auth implimentation module
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """class SessionAuth
+    """
+    pass
