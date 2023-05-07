0f6d86e42e302ddb25a9c7298bb53c20ec66517e
gims-inc <gabrielmusanga@gmail.com>
2023-05-07 04:19:11 +0300

    session-id : user-id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,19 @@
 """
 from api.v1.auth.auth import Auth
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """class SessionAuth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates session-id for a user-id
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
