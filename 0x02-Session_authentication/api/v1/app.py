8157654622761c1b72ec7fc76f043ad9ce648f17
gims-inc <gabrielmusanga@gmail.com>
2023-05-08 20:40:29 +0300

    Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,20 +56,16 @@ def not_found(error) -> str:
 def before_request() -> str:
     """Authenticate users before requests
     """
-    if auth is None:
-        return
-    else:
-        if not auth.require_auth(request.path, excluded_paths):
-            return
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
-
-        request.current_user = auth.current_user(request)
-
-        if request.current_user is None:
-            abort(403)
+    if auth:
+        path = request.path
+        if auth.require_auth(path, excluded_paths):
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+            else:
+                request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,9 @@ class SessionAuth(Auth):
         if not session_id or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """returns a user based on a cookie value
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
