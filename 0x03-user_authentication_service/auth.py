bc3dd6514ebe73e33ae61ec7fcb4df2ec541e867
gims-inc <gabrielmusanga@gmail.com>
2023-05-11 14:34:28 +0300

    reg user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,7 +28,8 @@ class Auth:
         return a User object.
         """
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
